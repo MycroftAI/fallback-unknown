5add94b53812a11ed2233beceedd7f0c84bdb1ce
Michael Hansen <hansen.mike@gmail.com>
2022-03-02 22:22:30 +0000

    Add "show speech to text"

--- __init__.py
+++ __init__.py
@@ -11,50 +11,85 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import time
+import typing
+from collections import deque
 
 from mycroft.skills.core import FallbackSkill
+from mycroft.skills import AdaptIntent, intent_handler
+from mycroft.util.log import LOG
+
+MAX_DISPLAY_UTTERANCES = 4
 
 
 class UnknownSkill(FallbackSkill):
     def __init__(self):
         super(UnknownSkill, self).__init__()
+        self.last_utterances = deque(maxlen=MAX_DISPLAY_UTTERANCES + 1)
 
     def initialize(self):
         self.register_fallback(self.handle_fallback, 100)
-        self.add_event('mycroft.speech.recognition.unknown',
-                       self.handle_unknown_recognition)
-        self.add_event('complete_intent_failure',
-                       self.handle_unknown_recognition)
+        self.add_event(
+            "mycroft.speech.recognition.unknown", self.handle_unknown_recognition
+        )
+        self.add_event("complete_intent_failure", self.handle_unknown_recognition)
+        self.add_event("recognizer_loop:utterance", self.handle_utterance)
 
     def read_voc_lines(self, name):
-        with open(self.find_resource(name + '.voc', 'vocab')) as f:
-            return filter(bool, map(str.strip, f.read().split('\n')))
+        with open(self.find_resource(name + ".voc", "vocab")) as f:
+            return filter(bool, map(str.strip, f.read().split("\n")))
 
     def handle_fallback(self, message):
         with self.activity():
-            utterance = message.data['utterance'].lower()
+            self.gui_show(self.last_utterances)
 
+            utterance = message.data["utterance"].lower()
             try:
-                self.report_metric('failed-intent', {'utterance': utterance})
+                self.report_metric("failed-intent", {"utterance": utterance})
             except Exception:
-                self.log.exception('Error reporting metric')
+                self.log.exception("Error reporting metric")
 
-            for i in ['question', 'who.is', 'why.is']:
+            for i in ["question", "who.is", "why.is"]:
                 for l in self.read_voc_lines(i):
                     if utterance.startswith(l):
-                        self.log.info('Fallback type: ' + i)
-                        self.speak_dialog(i, data={'remaining': l.replace(i, '')}, wait=True)
+                        self.log.info("Fallback type: " + i)
+                        self.speak_dialog(
+                            i, data={"remaining": l.replace(i, "")}, wait=True
+                        )
                         return True
 
             self.log.info(utterance)
-            self.speak_dialog('unknown', wait=True)
+            self.speak_dialog("unknown", wait=True)
+            time.sleep(5)
+            self.gui.release()
+
             return True
 
     def handle_unknown_recognition(self, message):
         """Called when no transcription is returned from STT"""
         with self.activity():
-            self.log.info('Unknown recognition')
-            self.speak_dialog('unknown', wait=True)
+            self.log.info("Unknown recognition")
+            self.speak_dialog("unknown", wait=True)
+
+    def handle_utterance(self, message):
+        utterances = message.data.get("utterances")
+        if utterances:
+            self.last_utterances.append(utterances[0])
+
+    @intent_handler(AdaptIntent().require("show").require("utterance"))
+    def show_last_utterance(self, _):
+        """Handles a user's request to show the most recent utterance."""
+        with self.activity():
+            self.gui_show(self.last_utterances)
+            time.sleep(5)
+            self.gui.release()
+
+    def gui_show(self, utterances: typing.Iterable[str]):
+        """Show speech to text utterance for debugging"""
+        for utt_idx, utt in enumerate(reversed(utterances)):
+            self.gui[f"utterance{utt_idx+1}"] = utt
+
+        self.gui.replace_page("utterance.qml", override_idle=True)
 
 
 def create_skill():
